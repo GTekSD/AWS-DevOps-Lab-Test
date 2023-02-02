80e8f79d45aa60ca59c641120d88b27b5baa8b86
Сухас Дхолз <55411358+GTekSD@users.noreply.github.com>
2023-02-02 23:33:14 +0530

    Update and rename twoin1.tf to main-twoin1.tf

---
+++ main-twoin1.tf
@@ -0,0 +1,68 @@
+#script to create an AWS instance as well as an ElasticSearch configuration
+""" To make the script in the example work, you need to replace the placeholders in the access policies and cognitooptions sections with your own credentials and access information. Additionally, you will need to update the subnetids and securitygroupids fields with the IDs of your own subnets and security groups. Finally, you will need to update the volumesize and automatedsnapshotstarthour fields with values that fit your needs. """
+
+provider "aws" {
+  access_key = "AKIASMYCEG4DVU"
+  secret_key = "rc4DXdBc8/YnD3JsHAcsaAaWMNzo78cE"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "Debian11" {
+  ami           = "ami-079b117c1800d30f8"
+  instance_type = "t2.micro"
+}
+
+
+#script that can be used to create a configuration for ElasticSearch that requires credentials and provides encrypted communication.
+
+resource "aws_elasticsearch_domain" "example" {
+  domain_name = "example-elasticsearch"
+
+  elasticsearch_version = "7.9"
+
+  cluster_config {
+    instance_type            = "t2.small.elasticsearch"
+    instance_count           = 2
+    dedicated_master_enabled = false
+    zone_awareness_enabled   = false
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  access_policies = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "es:*",
+      "Principal": "*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:es:us-east-1:*:domain/example-elasticsearch/*"
+    }
+  ]
+}
+POLICY
+  
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  vpc_options {
+    subnet_ids = ["subnet-12345678", "subnet-87654321"]
+    security_group_ids = ["sg-12345678"]
+  }
+
+  advanced_options = {
+    "rest.action.multi.allow_explicit_index" = "true"
+  }
+
+  cognito_options {
+    enabled = true
+    user_pool_id = "us-east-1_12345678"
+    identity_pool_id = "us-east-1:12345678-1234-1234-1234-123456789012"
+    role_arn = "arn:aws:iam::123456789012:role/Cognito_ExampleUnauth_Role"
+  }
+}
